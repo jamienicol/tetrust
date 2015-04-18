ea7d68a2834d8b1e999b57b6900a1841a09909fe
Jamie Nicol <jamie@thenicols.net>
2015-04-18 12:32:29 +0100

    init sdl2 and start main loop
    main loop clears the screen black and presents. exits on window quit or
    escape key.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::keycode::KeyCode;
+use sdl2::pixels::Color;
+use sdl2::render::{ Renderer, RenderDriverIndex, ACCELERATED, PRESENTVSYNC };
+use sdl2::video::{ Window, WindowPos, OPENGL };
+
 fn main() {
-    println!("Hello, world!");
+    let sdl = sdl2::init(sdl2::INIT_VIDEO).unwrap();
+
+    let window = match Window::new(
+        &sdl, "Tetrust",
+        WindowPos::PosCentered, WindowPos::PosCentered, 480, 960,
+        OPENGL)
+    {
+        Ok(window) => window,
+        Err(err) => panic!("failed to create window: {}", err)
+    };
+
+    let mut renderer = match Renderer::from_window(
+        window, RenderDriverIndex::Auto, ACCELERATED | PRESENTVSYNC)
+    {
+        Ok(renderer) => renderer,
+        Err(err) => panic!("failed to create renderer: {}", err)
+    };
+
+    let mut drawer = renderer.drawer();
+
+    let mut running = true;
+    while running {
+        for event in sdl.event_pump().poll_iter() {
+            match event {
+                Event::Quit {..} |
+                Event::KeyDown { keycode: KeyCode::Escape, .. } => {
+                    running = false
+                },
+                _ => {}
+            }
+        }
+
+        drawer.set_draw_color(Color::RGB(0, 0, 0));
+        drawer.clear();
+        drawer.present();
+    }
 }
